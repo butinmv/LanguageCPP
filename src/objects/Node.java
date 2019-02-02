1435471d352209d643182366176bbd72ce0129de
Maxim Butin <butinmv9722@gmail.com>
2019-02-03 00:12:10 +0700

    Доделать семантическое дерево

--- src/objects/Node.java
+++ src/objects/Node.java
@@ -16,4 +16,34 @@ public class Node {
         return typeObject;
     }
 
+    public static Node createVar(String name, TypeData typeData) {
+        Node node = new Node();
+        node.typeObject = TypeObject.VAR;
+        node.name = name;
+        node.typeData = typeData;
+        return node;
+    }
+
+    public static Node createFunction(String name, TypeData typeData, int count) {
+        Node node = new Node();
+        node.typeObject = TypeObject.FUNCTION;
+        node.name = name;
+        node.count = count;
+        node.typeData = typeData;
+        return node;
+    }
+
+    public static Node createEmptyNode() {
+        Node node = new Node();
+        node.typeObject = TypeObject.EMPTY;
+        return node;
+    }
+
+    public static Node createConst(TypeData typeData) {
+        Node node = new Node();
+        node.typeObject = TypeObject.CONST;
+        node.typeData = TypeData.UNKNOWN;
+        return node;
+    }
+
 }
